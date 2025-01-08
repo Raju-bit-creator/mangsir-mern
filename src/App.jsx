d4f8aefa080a83824d5faa18ebd746cb1cfd949d
Raju <rajutamang3456@gmail.com>
2025-01-08 16:57:00 +0545

    state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(2);
+  const [name, setName] = useState("ram");
+  const handleCount = () => {
+    setCount((count) => count + 1);
+  };
+  setTimeout(() => {
+    setName("hari");
+  }, 3000);
 
   return (
     <>
-      <div>
-        <h1> hello mangsir group</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Navbar title="hamro bazzar" />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="mt-4" onClick={handleCount}>
+          click me !
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="read-the-docs">yo click {count} times in button</p>
+      <p>
+        what is your friend name?<br></br>ans. {name}
       </p>
     </>
   );
